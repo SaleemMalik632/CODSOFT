509d1efac7d9557be52f60e181b2d7987ad48453
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-08-07 03:55:11 -0700

    Update SPAM SMS DETECTION.py

--- SPAM SMS DETECTION.py
+++ SPAM SMS DETECTION.py
@@ -3,11 +3,12 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 
-Data = pd.read_csv('spam.csv')
-X = Data['v2']
-ModelNumerical = CountVectorizer()
-NumericalData = ModelNumerical.fit_transform(X)
+Data = pd.read_csv('spam.csv') # Loading CSV File 
+X = Data['v2']  
+ModelNumerical = CountVectorizer() # Making the Object od the CountVectorizer bcz NB is work on the numricall 
+NumericalData = ModelNumerical.fit_transform(X)  # A Matrix of Numrical data is Found 
 
+# Spliting the Data in traing and testing pass 
 X_train, X_test, y_train, y_test = train_test_split(NumericalData, Data['v1']) 
 
 dense_X_train = X_train.toarray()
@@ -16,9 +17,10 @@ dense_X_test = X_test.toarray()
 
 Model = GaussianNB()
 Model.fit(dense_X_train, y_train) 
+# test for the preduction of the my model 
 text = "URGENT: Your bank account has been compromised. Click here to secure your account."
 NumText = ModelNumerical.transform([text]) 
-Test_text = NumText.toarray()  
+Test_text = NumText.toarray()   # again making the numrical matrix of the data 
 predictions = Model.predict(Test_text)
-print(predictions)  
-print(Model.score(dense_X_test, y_test)) 
+print(predictions)   # prediction of my model 
+print(Model.score(dense_X_test, y_test))  # Score and Accuracy of the model 
