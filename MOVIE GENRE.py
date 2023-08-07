690c416aa7c32a66beeff0c98919594240cd5285
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-08-07 04:02:42 -0700

    Create MOVIE GENRE.py

---
+++ MOVIE GENRE.py
@@ -0,0 +1,6 @@
+
+
+# ID ::: TITLE ::: GENRE ::: DESCRIPTION
+# 54214 ::: Nature's Fury: Storm of the Century (2006) ::: history ::: On Labor Day Weekend, 1935, the most intense hurricane to ever make landfall on America's shores hit the Florida Keys. The country was just starting to recover from the Great Depression when the ferocious beast with its 200 mile-an-hour winds and violent storm surge overpowered nearly everything in it's path.
+
+
