bed5f0104da614954c8b84152d731573e1650271
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-08-07 03:37:23 -0700

    Create CUSTOMER CHURN.py

---
+++ CUSTOMER CHURN.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+
+Data   = pd.read_csv('Churn_Modelling.csv') 
+
+Encoder  = LabelEncoder()
+Data['Gender'] = Encoder.fit_transform(Data['Gender']) 
+Data['Geography'] = Encoder.fit_transform(Data['Geography'])  
+
+X_train, X_test, y_train, y_test =  train_test_split(Data[['CreditScore','Geography','Gender','Age','Tenure','Balance','NumOfProducts','HasCrCard','IsActiveMember','EstimatedSalary']] , Data['Exited']  , test_size=0.2 ) 
+
+Model = LogisticRegression()
+Model.fit(X_train , y_train) 
+print(Model.predict(X_test)) 
+print(Model.score(X_test , y_test)*100)  
+ 
+print(Data['Gender'].array() )  
+
+
+  
+  
+
+
+
+
