2a6161f6e822a0773e611a8646866c354c4a3212
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-08-07 03:49:16 -0700

    Update CUSTOMER CHURN.py

--- CUSTOMER CHURN.py
+++ CUSTOMER CHURN.py
@@ -3,18 +3,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 
-Data   = pd.read_csv('Churn_Modelling.csv') 
+Data   = pd.read_csv('Churn_Modelling.csv')   # Loading the CSV file in the program 
 
-Encoder  = LabelEncoder()
-Data['Gender'] = Encoder.fit_transform(Data['Gender']) 
-Data['Geography'] = Encoder.fit_transform(Data['Geography'])  
+Encoder  = LabelEncoder()  # Object of the Encoder Bcz ML is only work on the Numrical Data 
+Data['Gender'] = Encoder.fit_transform(Data['Gender'])  # Encodeing the Gender 
+Data['Geography'] = Encoder.fit_transform(Data['Geography'])  #  Encodeing the Geography
+
+# Spliting The Data in Traing  and Testing parts 
 
 X_train, X_test, y_train, y_test =  train_test_split(Data[['CreditScore','Geography','Gender','Age','Tenure','Balance','NumOfProducts','HasCrCard','IsActiveMember','EstimatedSalary']] , Data['Exited']  , test_size=0.2 ) 
 
+
+# SimpleTest   = [[792,0,0,28,4,130142.79,1,1,0,38190.78]]
+
 Model = LogisticRegression()
-Model.fit(X_train , y_train) 
-print(Model.predict(X_test)) 
-print(Model.score(X_test , y_test)*100)  
+Model.fit(X_train , y_train)  # Training the Model 
+print(Model.predict(X_test))  #Prediction of The Model 
+print(Model.score(X_test , y_test)*100)   #prediction Score of the Model 
  
 
 
