63e4bcafac71a3ce5d1b0899a46728fb75f4cae3
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-08-07 23:18:26 -0700

    Update MOVIE GENRE.py

--- MOVIE GENRE.py
+++ MOVIE GENRE.py
@@ -1,10 +1,74 @@
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression 
 
-data =  pd.read_table('Genre Classification Dataset/test_data.txt')
+# Define the file path
+file_path = 'Genre Classification Dataset/train_data.txt'
+
+# Read the file line by line
+train_data = []
+with open(file_path, 'r', encoding='unicode_escape') as file:
+    for line in file:
+        line = line.strip()
+        if line:
+            parts = line.split(':::')
+            train_data.append(parts)
+
+# Define the column names
+columns = ['ID', 'TITLE', 'GENRE', 'DESCRIPTION']
+
+train_data = pd.DataFrame(train_data, columns=columns)
+
+title = train_data['TITLE']
+description = train_data['DESCRIPTION']
+
+
+# Combine title and description text into a single array
+text_data = title + ' ' + description 
+print(len(text_data))
+NumricalData = CountVectorizer()
+text_transformed = NumricalData.fit_transform(description)
+
+print(text_transformed.getnnz())  
+print(len(title))      
+print(len(train_data['GENRE']))
+
+# X_train, X_test, y_train, y_test = train_test_split( [[text_transformed]], train_data['GENRE'] , test_size=0.2)   
+
+# Model = LogisticRegression()
+# Model.fit(X_train , y_train) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Reading the testing data 
+# path = 'Genre Classification Dataset/test_data.txt'
+# test_data = []
+# with open(path , 'r'  , encoding='unicode_escape') as file:
+#     for line in file:
+#         line = line.strip()
+#         if line:
+#             parts  = line.split(':::')
+#             test_data.append(parts) 
+
  
-print(data) 
+# columns_ = ['ID', 'TITLE', 'DESCRIPTION']
+# test_data = pd.DataFrame(test_data , columns= columns_) 
+# print(test_data['DESCRIPTION']) 
 
-# ID ::: TITLE ::: GENRE ::: DESCRIPTION
-# 54214 ::: Nature's Fury: Storm of the Century (2006) ::: history ::: On Labor Day Weekend, 1935, the most intense hurricane to ever make landfall on America's shores hit the Florida Keys. The country was just starting to recover from the Great Depression when the ferocious beast with its 200 mile-an-hour winds and violent storm surge overpowered nearly everything in it's path.
+
+
 
 
